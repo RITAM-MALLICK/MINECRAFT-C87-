043d78a9ef3b5bcf1ea917e12bca6a276f0c5a2b
RITAM-MALLICK <87016544+RITAM-MALLICK@users.noreply.github.com>
2022-01-15 17:47:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+const { fabric } = require("./fabric");
+
+var canvas = new fabric.Canvas('myCanvas');
+
+player_x=10;
+player_y=10;
+
+block_image_width=30;
+block_image_height=30;
+
+var player_object="";
+
+function player_update() {
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+    })
+}
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if (e.shiftKey == true && keyPressed == '80') {
+        console.log("P And Shift Pressed Together");
+        block_image_width = block_image_width + 10;
+        block_image_height = block_image_height + 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if (e.shiftKey && keyPressed == '77') {
+        console.log("M And Shift Pressed Together");
+        block_image_width = block_image_width - 10;
+        block_image_height = block_image_height - 10;
+        document.getElementById("current_width").innerHTML = block_image_width;
+        document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    if (keyPressed == '38') {
+        up();
+        console.log("UP");
+    }
+    if (keyPressed == '40') {
+        down();
+        console.log("DOWN");
+    }
+    if (keyPressed == '37') {
+        left();
+        console.log("LEFT");
+    }
+    if (keyPressed == '39') {
+        right();
+        console.log("RIGHT");
+    }
+    if (keyPressed == '87') {
+        new_image('wall.jpg');
+        console.log("W");
+    }
+    if (keyPressed == '71') {
+        new_image('ground.png');
+        console.log("G");
+    }
+    if (keyPressed == '76') {
+        new_image('light_green.png');
+        console.log("L");
+    }
+    if (keyPressed == '84') {
+        new_image('trunk.jpg');
+        console.log("T");
+    }
+    if (keyPressed == '82') {
+        new_image('roof.jpg');
+        console.log("R");
+    }
+    if (keyPressed == '89') {
+        new_image('yellow_wall.png');
+        console.log("Y");
+    }
+    if (keyPressed == '68') {
+        new_image('dark_green.png');
+        console.log("D");
+    }
+    if (keyPressed == '85') {
+        new_image('unique.png');
+        console.log("U");
+    }
+    if (keyPressed == '67') {
+        new_image('cloud.jpg');
+        console.log("C");
+    }
+}
